dbb593ae4faf258c578107088764b6e352c644f0
Taras Samoilov <tarsam74@gmail.com>
2021-06-27 22:41:47 +0300

    Corrections were made after checking the task. Added script for automatic percent recalculation

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
 	menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__indicator-counter'),
+	lines = document.querySelectorAll('.skills__indicator-line span');
+
+counters.forEach((item, i) => {
+	lines[i].style.width = item.innerHTML;
+});
